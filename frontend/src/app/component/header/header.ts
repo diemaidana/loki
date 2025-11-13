ff6206fe36369262dc623cd2daa371cb2acf9d0c
LucasCamino05 <lucascamino05@gmail.com>
2025-11-13 13:27:47 -0300

    user form and service

--- frontend/src/app/component/header/header.ts
+++ frontend/src/app/component/header/header.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-header',
-  imports: [],
+  selector: 'header',
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
-
+  private readonly router = inject(Router);
 }
--- frontend/src/app/pages/user-form/user-form.ts
+++ frontend/src/app/pages/user-form/user-form.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { User } from '../../model/user';
+import { UserService } from '../../service/user-service';
 
 @Component({
   selector: 'app-user-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user-form.html',
   styleUrl: './user-form.css',
 })
 export class UserForm {
+  private readonly formBuilder = inject(FormBuilder);
+  private readonly service = inject(UserService);
+  protected readonly user = input<User>();
 
+  protected readonly form = this.formBuilder.nonNullable.group({
+    username: ["", [Validators.required]],
+    email: ["", [Validators.required]],
+    password: ["", [Validators.required]],
+    repassword: ["", [Validators.required]],
+    fullName: ["", [Validators.required]],
+    DNI: ["", [Validators.required]],
+    phoneNumber: ["", [Validators.required]],
+    address: ["", [Validators.required]],
+    nationality: ["", [Validators.required]]
+  });
+
+
+  handleSubmit(){
+    if(this.form.invalid){
+      alert("formulario invalido");
+      return
+    }
+    else{
+      const user = this.form.getRawValue();
+      this.service.postUser(user)
+    }
+  }
 }

2ddd44a10f3981880282dc2cbdc033190b64ec83
Diego Maidana <die.maidana1@gmail.com>
2025-11-14 13:14:40 -0300

    feat(home-page): styles done. fix: general fixes to header, footer and general styles.

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.html',
   styleUrl: './home.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Home {
-
+  items = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco', 'Seis', 'Siete', 'Ocho'];
 }

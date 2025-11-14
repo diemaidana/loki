c0c2d12d879f3a816153432f4b3b6dbab195b880
Diego Maidana <die.maidana1@gmail.com>
2025-11-14 14:50:43 -0300

    fix(footer): change footer selector. From 'footer' to 'app-footer'

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./component/header/header";
-import { Footer } from "./component/footer/footer";
+import { Footer } from './component/footer/footer';
 
 @Component({
   selector: 'app-root',

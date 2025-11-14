29dbae280e45fc1dbb2bd9f2cf686c648b31063e
Diego Maidana <die.maidana1@gmail.com>
2025-11-14 13:48:07 -0300

    fix(global): general changes to styles

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,9 +1,9 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import Swiper from 'swiper';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]

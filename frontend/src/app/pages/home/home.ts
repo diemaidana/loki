4eb6c89ea9116f2157f9032cb2d94ce328ce8d79
Diego Maidana <die.maidana1@gmail.com>
2025-11-15 13:00:02 -0300

    feat(home-page): home-page finished.

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,13 +1,18 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, effect, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ProductService } from '../../service/product';
+import { Product } from '../../model/product';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, CurrencyPipe],
   templateUrl: './home.html',
   styleUrl: './home.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Home {
-  items = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco', 'Seis', 'Siete', 'Ocho'];
+  private readonly productsService = inject(ProductService);
+  protected readonly products = toSignal(this.productsService.getProducts(), { initialValue: [] });
 }

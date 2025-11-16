5d19d9028b50ed9deffc84c85b157bdb19672272
Diego Maidana <die.maidana1@gmail.com>
2025-11-16 11:47:30 -0300

    feat(list-product): next and previous page buttons working.

--- frontend/src/app/pages/list-products/list-products.ts
+++ frontend/src/app/pages/list-products/list-products.ts
@@ -11,14 +11,14 @@ import { CurrencyPipe } from '@angular/common';
 })
 export class ListProducts {
   private readonly http = inject(ProductService);
-  protected readonly products = toSignal(this.http.getProducts());
+  protected readonly products = toSignal(this.http.getProducts(), { initialValue: []});
 
   protected categories = computed(() => {
     const prods = this.products();
     return [...new Set(prods?.map(p => p.category))];
   })
 
-  private currentPage = signal(1);
+  protected currentPage = signal(1);
   private pageSize = 9;
 
   protected paginatedProducts = computed(() => {
@@ -30,4 +30,19 @@ export class ListProducts {
 
     return this.products()?.slice(start, end);
   })
+
+  previousPage() {
+    if(this.currentPage() > 1){
+      this.currentPage.update(page => page - 1);
+    }
+  }
+
+  nextPage() {
+    const pages = Math.ceil(this.products().length / this.pageSize);
+    if(this.currentPage() < pages){
+      this.currentPage.update(page => page + 1);
+    }
+  }
+
+
 }

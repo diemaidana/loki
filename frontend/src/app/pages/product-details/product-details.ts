99fdbc8d7f0b3f27a8cc90ca3b10a1b3d100dfe8
Diego Maidana <die.maidana1@gmail.com>
2025-11-16 23:09:23 -0300

    feat(product-detail): styles done.

--- frontend/src/app/pages/product-details/product-details.ts
+++ frontend/src/app/pages/product-details/product-details.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { ProductService } from '../../service/product';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-details',
-  imports: [RouterLink],
+  imports: [RouterLink, CurrencyPipe],
   templateUrl: './product-details.html',
   styleUrl: './product-details.css',
 })
 export class ProductDetails {
-  
+  private readonly productService = inject(ProductService);
+  private readonly router = inject(Router);
+  private readonly route = inject(ActivatedRoute);
+
+  private readonly id = this.route.snapshot.paramMap.get('id');
+
+  protected product = toSignal(this.productService.getProductsById(this.id!));
+
+
 }
